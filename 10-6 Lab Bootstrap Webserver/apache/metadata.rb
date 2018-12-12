468919318b5ebb1a1284a9587a66abd5acc45d83
stellarsquall <stellarsquall@protonmail.ch>
2018-12-12 16:19:43 -0600

    updated section 10 module numbers to add Disclaimer Video

--- 10-6 Lab Bootstrap Webserver/apache/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'apache'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures apache'
-long_description 'Installs/Configures apache'
-version          '0.2.1'
-
