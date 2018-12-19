ffcb70324fb6e9c6708917cbd6860c10faf6e334
stellarsquall <stellarsquall@protonmail.ch>
2018-12-18 19:31:40 -0600

    initial workstation cookbook commit

---
+++ 04-3 cookbook components/end/workstation/recipes/setup.rb
@@ -0,0 +1,12 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+	content 'This server is the property of ...'
+	action :create
+	owner 'root'
+	group 'root'
+end
---
+++ 04-3 cookbook components/end/workstation/test/recipes/default_test.rb
@@ -0,0 +1,19 @@
+# # encoding: utf-8
+
+# Inspec test for recipe apache::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec_reference.html
+
+unless os.windows?
+  # This is an example test, replace with your own test.
+  describe user('root'), :skip do
+    it { should exist }
+  end
+end
+
+# This is an example test, replace it with your own test.
+describe port(80), :skip do
+  it { should_not be_listening }
+end
+ 
