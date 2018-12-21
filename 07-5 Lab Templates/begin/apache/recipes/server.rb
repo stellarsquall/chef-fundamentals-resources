cdadc4754e7d28ffce8f55d378525112910d6f12
stellarsquall <stellarsquall@protonmail.ch>
2018-12-20 18:19:11 -0600

    initial commit

---
+++ 07-5 Lab Templates/begin/apache/recipes/server.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'httpd' do
+	action :install
+end
+
+file '/var/www/html/index.html' do
+	content "<h1>Hello, world!</h1>
+  <h2>IPADDRESS: #{node['ipaddress']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+	"
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
