6a5a7f772ed43882b6ee8896784750cc7f946b2a
stellarsquall <stellarsquall@protonmail.ch>
2018-12-20 19:14:52 -0600

    initial commit

---
+++ 14-4 Dynamic LB/end/myhaproxy/metadata.rb
@@ -0,0 +1,9 @@
+name 'myhaproxy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myhaproxy'
+long_description 'Installs/Configures myhaproxy'
+version '0.1.0'
+
+depends 'haproxy', '= 2.0.0'
