d9cece7ad18646d633dc2ab940a08524e8b773cf
stellarsquall <stellarsquall@protonmail.ch>
2018-12-20 19:17:32 -0600

    initial commit

---
+++ 15-5 Refine LB/begin/myhaproxy/metadata.rb
@@ -0,0 +1,9 @@
+name 'myhaproxy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myhaproxy'
+long_description 'Installs/Configures myhaproxy'
+version '1.0.0'
+
+depends 'haproxy', '= 2.0.0'
