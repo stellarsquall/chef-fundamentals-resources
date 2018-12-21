615352c2617fc55e83cabaa4c73f1a875c34d3de
stellarsquall <stellarsquall@protonmail.ch>
2018-12-20 18:22:15 -0600

    initial commit

---
+++ 08-2 cookbook_file/end/apache/recipes/server.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'httpd' do
+	action :install
+end
+
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
+end
+
+# template '/var/www/html/index.html' do
+#   source 'index.html.erb'
+# end
+
+service 'httpd' do
+	action [:enable,:start]
+end
