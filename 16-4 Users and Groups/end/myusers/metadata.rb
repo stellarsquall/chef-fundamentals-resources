13dbeba6c02a146b7347bf51d8f71d31d38cf983
stellarsquall <stellarsquall@protonmail.ch>
2018-12-20 19:20:30 -0600

    initial commit

---
+++ 16-4 Users and Groups/end/myusers/metadata.rb
@@ -0,0 +1,7 @@
+name 'myusers'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myusers'
+long_description 'Installs/Configures myusers'
+version '0.1.0'
