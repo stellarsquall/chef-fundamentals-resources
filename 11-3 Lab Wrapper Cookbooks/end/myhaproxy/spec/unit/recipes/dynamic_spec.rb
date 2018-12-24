b4d03c8d8c51b90889114df57f896bc618d2a76d
stellarsquall <stellarsquall@protonmail.ch>
2018-12-24 14:35:18 -0600

    matched to 1.2.0 video demo release

--- 11-3 Lab Wrapper Cookbooks/end/myhaproxy/spec/unit/recipes/dynamic_spec.rb
+++ 11-3 Lab Wrapper Cookbooks/end/myhaproxy/spec/unit/recipes/dynamic_spec.rb
@@ -1,12 +1,12 @@
 #
 # Cookbook:: myhaproxy
-# Spec:: default
+# Spec:: dynamic
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'myhaproxy2::default' do
+describe 'myhaproxy2::dynamic' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
